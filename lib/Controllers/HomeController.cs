f36d4bac73c5906c2237850345a2b72d4ed374ee
us100 <49002778+us100@users.noreply.github.com>
2020-05-28 02:32:09 +0500

    Update HomeController.cs

--- lib/Controllers/HomeController.cs
+++ lib/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Linq.Mapping;
 using lib.Models;
+using System.Drawing;
 
 namespace lib.Controllers
 {
@@ -24,35 +25,37 @@ namespace lib.Controllers
             String name = Request.Form["name"];
             String author = Request.Form["author"];
             String co = Request.Form["copies"];
+            String cat = Request.Form["cat"];
             int copies = int.Parse(co);
             book b = new book();
             b.name = name;
             b.author = author;
             b.copies = copies;
+            b.category = cat;
             boo.books.InsertOnSubmit(b);
             boo.SubmitChanges();
             return RedirectToAction("BookAdded");
         }
-
+        public ActionResult show()
+        {
+            return View();
+        }
         public ActionResult showbook()
         {
+            String cat = Request.Form["cat"];
             List<book> lists = boo.books.ToList<book>();
             List<book> list = new List<book>();
             if (lists != null)
             {
                 foreach (var q in lists)
                 {
-                    list.Add(q);
+                    if (q.category == cat)
+                        list.Add(q);
                 }
             }
             if (list != null)
-            {
                 return View(list);
-            }
-            else
-            {
-                return View(); 
-            }
+            else return View();
         }
         public ActionResult BookAdded()
         {
@@ -94,6 +97,7 @@ namespace lib.Controllers
             String bookname = Request["bookname"];
             String author = Request["author"];
             String co = Request["copies"];
+            String cat = Request["cat"];
             int copies = Convert.ToInt32(co);
             String no = Request["id"];
             int id = Convert.ToInt32(no);
@@ -102,6 +106,7 @@ namespace lib.Controllers
             b.author = author;
             b.copies = copies;
             b.name = bookname;
+            b.category = cat;
             boo.SubmitChanges();
             return RedirectToAction("bookupdated");
         }
